f6a1e22515be64ef825172205c498520709796a2
Kelly Crowther <crowther.kelly1@gmail.com>
2018-02-11 14:45:34 -0800

    api returning get request data from mysql

--- server.js
+++ server.js
@@ -6,31 +6,32 @@ var mysql = require('mysql');
 var databaseConfig = require('./database-connection-config.json');
 var connection = mysql.createConnection(databaseConfig);
 
-// simple get to test express is working
+// returning data from the root
+
 app.get('/', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+    // connect to the database and catch any errors
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+
+        console.log('connected as id ' + connection.threadId);
+    });
+
+    // simple query to make sure we are retrieving data from mysql
+    connection.query('SELECT * FROM availableActivity', function (err, rows, fields) {
+        if (err) throw err;
+
+        console.log('availableActivity is: ', rows[1].activityName);
+
+        // response to procure for the get request
+        res.send(rows);
+    });
+
+    // close the connection 
+    connection.end();
 });
-
-// connect to the database and catch any errors
-connection.connect(function (err) {
-    if (err) {
-        console.error('my error connecting: ' + err.stack);
-        return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-});
-
-// simple query to make sure we are retrieving data from mysql
-connection.query('SELECT * FROM availableActivity', function (err, rows, fields) {
-    // if (err) throw err
-
-    console.log('availableActivity is: ', rows[1].activityName);
-});
-
-// close the connection 
-connection.end();
-
 
 app.listen(port);
 
