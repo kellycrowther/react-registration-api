b653d2496c163900c06c6e2449ec6bde5b922ba1
Kelly Crowther <crowther.kelly1@gmail.com>
2018-03-17 21:22:14 -0700

    create single dateTime piece of data and transform T/F for MySql

--- routes/activities/single.js
+++ routes/activities/single.js
@@ -6,12 +6,18 @@ var mysql = require('mysql');
 // post to availableActivity table
 module.exports = app.post('/', (req, res) => {
 
-    // console.log('postman: ', req.body);
+    var date = req.body.date;
+    var time = req.body.time;
+
+    var dateTime = createDateTime(date, time);
+    var canEdit = transformTrueFalse(req.body.canEdit);
+
+    // res.status(200);
 
     var query = "INSERT INTO ??(??,??, ??, ??, ??, ??) VALUES (?,?,?,?,?,?)";
     var table = [
       "availableActivity", "activityName", "date", "location", "ageRestriction", "price", "canEdit",
-      req.body.activityName, req.body.date, req.body.location, req.body.ageRestriction, req.body.price, req.body.canEdit
+      req.body.activityName, dateTime, req.body.location, req.body.ageRestriction, req.body.price, canEdit
     ];
     query = mysql.format(query, table);
     connection.query(query, function (err, rows) {
@@ -24,3 +30,21 @@ module.exports = app.post('/', (req, res) => {
       }
     })
   });
+
+  function createDateTime(date, time) {
+    var year = date.slice(0, 4);
+    var month = date.slice(5, 7);
+    month = parseInt(month) - 1;
+
+    var day = date.slice(8, 10);
+
+    var hours = time.slice(0, 2);
+    var minutes = time.slice(3, 5);
+
+    var dateTime = new Date(year, month, day, hours, minutes);
+    return dateTime;
+  }
+
+  function transformTrueFalse(canEdit){
+    return (canEdit ? 1 : 0);
+  }
