9439375c2f6dc232fc948f26e2697845f0c61584
Kelly Crowther <crowther.kelly1@gmail.com>
2018-05-03 21:48:34 -0700

    register route added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const activities = require('./activities');
 const login = require('./users/login');
+const register = require('./users/register');
 
 routes.get('/', (req, res) => {
     res.status(200).json({ message: 'Connected!' });
@@ -8,5 +9,6 @@ routes.get('/', (req, res) => {
 
 routes.use('/activities', activities);
 routes.use('/login', login);
+routes.use('/register', register);
 
 module.exports = routes;
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,8 @@
 const users = require('express').Router();
 const login = require('./login');
+const register = require('./register');
 
 users.post('/login', login);
+users.post('/register', register);
 
 module.exports = users;
