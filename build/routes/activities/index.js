b8a1a1d66ba9ff710a522422a32296b0894404aa
Kelly Crowther <crowther.kelly1@gmail.com>
2018-07-15 17:20:12 -0700

    Revert "feature/typescript: typescript added with gulp build"
    This reverts commit 32d406b85275e458d156538def8ef80fcf427ee1.

--- build/routes/activities/index.js
+++
@@ -1,8 +0,0 @@
-var activities = require('express').Router();
-var all = require('./all');
-var single = require('./single');
-var passport = require('passport');
-var authorize = require('../../middleware/authorize');
-activities.get('/', all);
-activities.post('/', passport.authenticate('jwt', { session: false }), authorize.authorize, single);
-module.exports = activities;
