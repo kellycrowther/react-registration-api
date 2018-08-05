516d1a43f51259df2cf3825ee98eaa0c3e521283
Kelly Crowther <crowther.kelly1@gmail.com>
2018-08-04 22:18:00 -0700

    add back in auth for posting activity

--- src/routes/activities/index.ts
+++ src/routes/activities/index.ts
@@ -2,14 +2,14 @@ const activities = require('express').Router();
 const get = require('./get');
 const post = require('./post');
 const activity = require('./activity');
-// var passport = require('passport');
-// var authorize = require('../../middleware/authorize');
+var passport = require('passport');
+var authorize = require('../../middleware/authorize');
 import * as updateRoute from './activity/availability/update';
 
 let update: updateRoute.Route.UpdateActivity = new updateRoute.Route.UpdateActivity();
 
 activities.get('/', get);
-activities.post('/', post);
+activities.post('/', passport.authenticate('jwt', { session: false }), authorize.authorize, post);
 activities.put('/:activityId/availability/:availabilityId', update.update.bind(update.update));
 activities.delete('/:activityId', activity);
 
